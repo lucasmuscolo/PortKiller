0962d4ab4cfde2b7ed55088f932922a8a4de4b7e
Luking <lucasmuscolo@gmail.com>
2022-05-09 17:50:01 -0300

    bug fix

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,12 +2,15 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class Controller {
 
+    @FXML
+    private Label _etiqueta;
     @FXML
     private Button _boton;
 
@@ -19,9 +22,12 @@ public class Controller {
     {//"netstat -ano|findstr \"PID : "+_portn
         Runtime rt = Runtime.getRuntime();
         String numero = String.valueOf(_numero.getText());
+        String cmd = "netstat -ano|findstr \"PID : "+numero+"\"";
+        System.out.println(cmd);
         try {
-            Process pr = rt.exec("netstat -ano|findstr \"PID : "+numero);
+            Process pr = rt.exec(cmd);
             _numero.setText("");
+            _etiqueta.setText("You killed that port");
         } catch(IOException e) {
             e.printStackTrace();
         }
